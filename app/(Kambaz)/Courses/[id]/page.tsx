efab0b1dc16411fce6fc479fb728c2eed6825672
Jose Annunziato <jannunzi@gmail.com>
2025-07-13 01:18:04 -0400

    added promise

--- app/(Kambaz)/Courses/[id]/page.tsx
+++ app/(Kambaz)/Courses/[id]/page.tsx
@@ -1,5 +1,10 @@
-import { redirect } from "next/dist/client/components/navigation";
+import { redirect } from "next/navigation";
 
-export default function CoursesPage({ params }: { params: { id: string } }) {
-  redirect(`/Courses/${params.id}/Home`);
+export default async function CoursesPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params; // Await to resolve the Promise
+  redirect(`/Courses/${id}/Home`);
 }
