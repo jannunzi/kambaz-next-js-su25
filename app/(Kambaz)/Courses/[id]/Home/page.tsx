3d82aecb408d3e425c8784219e84e5ca52534261
Jose Annunziato <jannunzi@gmail.com>
2025-07-13 13:35:16 -0400

    added home title

--- app/(Kambaz)/Courses/[id]/Home/page.tsx
+++ app/(Kambaz)/Courses/[id]/Home/page.tsx
@@ -2,15 +2,18 @@ import Modules from "../Modules/page";
 import CourseStatus from "./Status";
 export default function Home() {
   return (
-    <table id="wd-home">
-      <tr>
-        <td valign="top" width="70%">
-          <Modules />
-        </td>
-        <td valign="top">
-          <CourseStatus />
-        </td>
-      </tr>
-    </table>
+    <div>
+      <h3>Home</h3>
+      <table id="wd-home">
+        <tr>
+          <td valign="top" width="70%">
+            <Modules />
+          </td>
+          <td valign="top">
+            <CourseStatus />
+          </td>
+        </tr>
+      </table>
+    </div>
   );
 }
